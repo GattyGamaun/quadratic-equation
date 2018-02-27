b7ca1eaa620bf28581a6b23edc1dc3f0066e9e25
Sergio Kondratiuk <gattygamaun@gmail.com>
2018-02-27 15:03:42 +0300

    Update index.js
    The first solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  let str = equation;
+  let substr1 = str.replace('^2', '');
+  let substr2 = substr1.replace(/\s/g, "");
+  let ar = substr2.split('*x');
+
+  let a = ar[0];
+  let b = ar[1];
+  let c = ar[2];
+
+  let diskr = b * b - 4 * a * c;
+  let sqrt = Math.sqrt(diskr);
+  let findx1 = (-b + Math.sqrt(diskr)) / (2 * a);
+  let findx2 = (-b - Math.sqrt(diskr)) / (2 * a);
+  let fidnx = -b / (2 * a);
+
+  if (findx1 < findx2) {
+    let mas = [Math.round(findx1), Math.round(findx2)];
+    return mas;
+  } else {
+    let mas = [Math.round(findx2), Math.round(findx1)];
+    return mas;
+  }
 }
